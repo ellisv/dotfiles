bbc0baac7448af36cf318c8daa71c02be4805185
Eligijus Vitkauskas <eligijusvitkauskas@gmail.com>
2016-03-29 21:10:39 +0300

    Catch an exception whenever spotify is not reachable

--- scripts/spotify-current.py
+++ scripts/spotify-current.py
@@ -1,10 +1,13 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 
-import dbus
+import dbus, sys
 
 session_bus = dbus.SessionBus()
-spotify_bus = session_bus.get_object('org.mpris.MediaPlayer2.spotify', '/org/mpris/MediaPlayer2')
+try:
+    spotify_bus = session_bus.get_object('org.mpris.MediaPlayer2.spotify', '/org/mpris/MediaPlayer2')
+except dbus.exceptions.DBusException:
+    sys.exit(1)
 spotify_properties = dbus.Interface(spotify_bus, 'org.freedesktop.DBus.Properties')
 metadata = spotify_properties.Get('org.mpris.MediaPlayer2.Player', 'Metadata')
 
