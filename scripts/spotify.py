4f323508b61d1434f151a090d116e9d915081803
Eligijus Vitkauskas <eligijusvitkauskas@gmail.com>
2016-08-01 20:54:33 +0300

    Use argparse for parameter parsing in spotify.py

--- scripts/spotify.py
+++ scripts/spotify.py
@@ -3,54 +3,56 @@
 
 import sys
 import dbus
+import argparse
 
-def busaware(func):
-    try:
-        bus = dbus.SessionBus().get_object('org.mpris.MediaPlayer2.spotify', '/org/mpris/MediaPlayer2')
-    except dbus.exceptions.DBusException:
-        sys.exit(1)
+class SpotifyControl:
+    def __init__(self):
+        self.bus = dbus.SessionBus().get_object('org.mpris.MediaPlayer2.spotify', '/org/mpris/MediaPlayer2')
 
-    return lambda *args: func(bus, *args)
+    def property(self, name):
+        interface = dbus.Interface(self.bus, 'org.freedesktop.DBus.Properties')
+        return interface.Get('org.mpris.MediaPlayer2.Player', name)
 
-@busaware
-def get_player(bus):
-    return dbus.Interface(bus, 'org.mpris.MediaPlayer2.Player')
-
-@busaware
-def get_property(bus, name):
-    interface = dbus.Interface(bus, 'org.freedesktop.DBus.Properties')
-    return interface.Get('org.mpris.MediaPlayer2.Player', name)
-
-
-if __name__ == '__main__':
-    try:
-        arg = sys.argv[1]
-    except IndexError:
-        print('Command was not specified')
-        sys.exit(2)
-
-    if arg == 'current':
-        playing = get_property('PlaybackStatus') == 'Playing'
+    def print_current(self):
+        playing = self.property('PlaybackStatus') == 'Playing'
         if not playing:
-            sys.exit(0)
+            return
 
-        metadata = get_property('Metadata')
+        metadata = self.property('Metadata')
         title = metadata['xesam:artist'][0] + ' - ' + metadata['xesam:title']
         trimmed_title = (title[:35] + '...') if len(title) > 38 else title
 
         print '♫', trimmed_title
-    elif arg == 'play':
-        get_player().Play()
-    elif arg == 'pause':
-        get_player().Pause()
-    elif arg == 'toggle':
-        get_player().PlayPause()
-    elif arg == 'next':
-        get_player().Next()
-    elif arg == 'previous':
-        get_player().Previous()
-    elif arg == 'stop':
-        get_player().Stop()
-    else:
-        print 'Unknown command'
-        sys.exit(3)
+
+    def send(self, signal):
+        player = dbus.Interface(self.bus, 'org.mpris.MediaPlayer2.Player')
+        getattr(player, signal)()
+
+
+def main():
+    parser = argparse.ArgumentParser()
+    subparsers = parser.add_subparsers(metavar='action')
+
+    parser_current = subparsers.add_parser('current', help='Print out a current song')
+    parser_current.set_defaults(func=SpotifyControl.print_current)
+
+    parser_toggle = subparsers.add_parser('toggle', help='Toggle between Play/Pause')
+    parser_toggle.set_defaults(func=lambda spotify: spotify.send('PlayPause'))
+
+    parser_next = subparsers.add_parser('next', help='Play a next song')
+    parser_next.set_defaults(func=lambda spotify: spotify.send('Next'))
+
+    parser_previous = subparsers.add_parser('previous', help='Play a previous song')
+    parser_previous.set_defaults(func=lambda spotify: spotify.send('Previous'))
+
+    args = parser.parse_args()
+
+    try:
+        spotify = SpotifyControl()
+        args.func(spotify)
+    except dbus.exceptions.DBusException:
+        sys.exit(1)
+
+
+if __name__ == '__main__':
+    main()
